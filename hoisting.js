a346d40d89bd2069e5431a3a15976d74dada010b
E.B <edenbenbow@yahoo.com>
2019-07-11 03:19:55 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -1,8 +1,11 @@
 function callMe() {
-  console.log("maybe");
+  var lyric = "maybe";
+  console.log("I just met you...");
+  console.log("and this is crazy..");
+  console.log("but here's my number..");
+  console.log("so call me");
+  return lyric;
 }
-
-callMe();
 
 
 function crazy() {
